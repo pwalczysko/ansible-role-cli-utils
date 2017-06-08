d39a87cd7934831a5ae1847076794d070a427d23
jmoore <josh@glencoesoftware.com>
2017-06-08 13:50:29 +0200

    Updates from Simon

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,9 +4,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_hosts_file(File):
-    f = File('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_analysis_packages(Package):
+    assert Package('bzip2')
